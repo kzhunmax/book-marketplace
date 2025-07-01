ff1a189a48136e8897c387ec06a51b272b3c86fc
Maksym Korshun <korshunmax80@gmail.com>
2025-07-01 15:06:11 +0200

    Add Book entity with basic fields

---
+++ src/main/java/com/example/bookmanager/model/Book.java
@@ -0,0 +1,22 @@
+package com.example.bookmanager.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "books")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String author;
+    private double price;
+    private String description;
+}
