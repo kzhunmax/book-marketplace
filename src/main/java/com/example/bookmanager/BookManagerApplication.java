a4e6e44fa5728846c9d942331395f46baac4b1d5
Maksym Korshun <korshunmax80@gmail.com>
2025-07-02 02:31:41 +0200

    Added vault configuration to application.properties, fixed class naming of BookManagerApplication class

--- src/main/java/com/example/bookmanager/BookManagerApplication.java
+++ src/main/java/com/example/bookmanager/BookManagerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookmanagerApplication {
+public class BookManagerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookmanagerApplication.class, args);
+		SpringApplication.run(BookManagerApplication.class, args);
 	}
 
 }
